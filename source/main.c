dc1fbf30e4d074e79e9203ed13be0a1183068c60
ethylamine <46206558+ethylamine@users.noreply.github.com>
2020-07-17 06:18:03 +0000

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,16 +1,14 @@
-#define DEBUG_SOCKET 1
-
 #include "ps4.h"
 #include "defines.h"
 
-#define    KERN_XFAST_SYSCALL 0x30EB30
+#define    KERN_XFAST_SYSCALL 0x1c0
 
 #define	CTL_KERN	1	/* "high kernel": proc, limits */
 #define	KERN_PROC	14	/* struct: process entries */
 #define	KERN_PROC_VMMAP	32	/* VM map entries for process */
 #define	KERN_PROC_PID	1	/* by process id */
 
-extern char kexec[];
+extern char kexec_data[];
 extern unsigned kexec_size;
 
 static int sock;
@@ -33,7 +31,6 @@ unsigned int long long __readmsr(unsigned long __register) {
 int kpayload(struct thread *td, struct kpayload_args* args){
 
 	//Starting kpayload...
-
 	struct ucred* cred;
 	struct filedesc* fd;
 
@@ -41,16 +38,20 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	cred = td->td_proc->p_ucred;
 
 	//Reading kernel_base...
-	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-0x30EB30];
+	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-KERN_XFAST_SYSCALL];
 	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
-	void** got_prison0 =   (void**)&kernel_ptr[0xF26010];
-	void** got_rootvnode = (void**)&kernel_ptr[0x206D250];
-	
-	//Resolve kernel functions...
-	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kernel_base + 0x286d70);
-	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x347580);
-	int (*copyin)(const void *uaddr, void *kaddr, size_t len) = (void *)(kernel_base + 0x286df0);
+	void** got_prison0 =   (void**)&kernel_ptr[0x113E518];
+	void** got_rootvnode = (void**)&kernel_ptr[0x2300320];
 
+	//Resolve kernel functions...
+	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kernel_base + 0x3C16B0);
+	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x123280);
+	int (*set_nclk_mem_spd)(int val) = (void *)(kernel_base + 0x19A390);
+	int (*set_pstate)(int val) = (void *)(kernel_base + 0x4FC2E0);
+	int (*set_gpu_freq)(int cu, unsigned int freq) = (void *)(kernel_base + 0x4E0530);
+	int (*update_vddnp)(unsigned int cu) = (void *)(kernel_base + 0x4E0B10);
+	int (*set_cu_power_gate)(unsigned int cu) = (void *)(kernel_base + 0x4E0F20);
+	
 	cred->cr_uid = 0;
 	cred->cr_ruid = 0;
 	cred->cr_rgid = 0;
@@ -58,25 +59,42 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 
 	cred->cr_prison = *got_prison0;
 	fd->fd_rdir = fd->fd_jdir = *got_rootvnode;
-
+	
+	//CLK, CU, ..
+	set_pstate(3);
+	set_nclk_mem_spd(8);
+	
+	set_gpu_freq(0, 800); // ACLK
+	set_gpu_freq(1, 674); //
+	set_gpu_freq(2, 610);
+	set_gpu_freq(3, 800);
+	set_gpu_freq(4, 800);
+	set_gpu_freq(5, 720);
+	set_gpu_freq(6, 720);
+	
+	update_vddnp(0x12);
+	set_cu_power_gate(0x12);
+	
 	//Disable write protection...
 	uint64_t cr0 = readCr0();
 	writeCr0(cr0 & ~X86_CR0_WP);
 	
+	kernel_ptr[0x20734E] = 3; //6.72 pstate when shutdown
+
 	//Kexec init
-	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xA0DFF8);
-	memcpy(DT_HASH_SEGMENT,kexec, kexec_size);
+	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xB1D820); // I know it's for 4.55 but I think it will works, not sure about this one on 6.72
+	memcpy(DT_HASH_SEGMENT, kexec_data, kexec_size);
 
 	void (*kexec_init)(void *, void *) = DT_HASH_SEGMENT;
 
-	kexec_init((void *)(kernel_base+0x347580), NULL);
+	kexec_init((void *)(kernel_base+0x123280), NULL); // this leads to printf? ok then
 
 	// Say hello and put the kernel base in userland to we can use later
-	printfkernel("PS4 Linux Loader for 4.05\n");
+	printfkernel("PS4 Linux Loader for 6.72 by valentinbreiz\n");
 
 	printfkernel("kernel base is:0x%016llx\n", kernel_base);
 
-	uint64_t uaddr;
+	void* uaddr;
 	memcpy(&uaddr,&args[2],8);
 
 	printfkernel("uaddr is:0x%016llx\n", uaddr);
@@ -88,35 +106,37 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 
 int _main(struct thread *td) {
 
-	initKernel();	
+	initKernel();
 	initLibc();
 	initNetwork();
 	initPthread();
-
+	initSysUtil();
+	
+#ifdef DEBUG_SOCKET
 	struct sockaddr_in server;
 
 	server.sin_len = sizeof(server);
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = IP(192, 168, 1, 12);
+	server.sin_addr.s_addr = IP(DIP_1, DIP_2, DIP_3, DIP_4); //DEBUG_IPADDRESS);
 	server.sin_port = sceNetHtons(9023);
 	memset(server.sin_zero, 0, sizeof(server.sin_zero));
 	sock = sceNetSocket("debug", AF_INET, SOCK_STREAM, 0);
 	sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server));
 	int flag = 1;
 	sceNetSetsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(int));
-	
+#endif
 	printfsocket("Connected!\n");
 
 	uint64_t* dump = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
 	printfsocket("Starting kernel patch...\n");
 
-	int sRet = syscall(11,kpayload,td,dump);
+	syscall(11,kpayload,td,dump);
 
 	printfsocket("Kernel patched, Kexec initialized!\n");
 
 	printfsocket("Starting PS4 Linux Loader\n");
-	
+
 	usbthing();
 
 	printfsocket("Done!\n");
@@ -127,17 +147,35 @@ int _main(struct thread *td) {
 
 }
 
+void notify(char *message)
+{
+	char buffer[512];
+	sprintf(buffer, "%s\n\n\n\n\n\n\n", message);
+	sceSysUtilSendSystemNotificationWithText(0x81, buffer);
+}
+
 void usbthing()
 {
 
 	printfsocket("Open bzImage file from USB\n");
 	FILE *fkernel = fopen("/mnt/usb0/bzImage", "r");
+	if(!fkernel)
+	{
+		notify("Error: open /mnt/usb0/bzImage.");
+		return;
+	}
 	fseek(fkernel, 0L, SEEK_END);
 	int kernelsize = ftell(fkernel);
 	fseek(fkernel, 0L, SEEK_SET);
 
 	printfsocket("Open initramfs file from USB\n");
 	FILE *finitramfs = fopen("/mnt/usb0/initramfs.cpio.gz", "r");
+	if(!finitramfs)
+	{
+		notify("Error: open /mnt/usb0/initramfs.cpio.gz");
+		fclose(fkernel);
+		return;
+	}
 	fseek(finitramfs, 0L, SEEK_END);
 	int initramfssize = ftell(finitramfs);
 	fseek(finitramfs, 0L, SEEK_SET);
@@ -154,10 +192,15 @@ void usbthing()
 	}
 
 	void *kernel, *initramfs;
-	char *cmd_line = "panic=0 clocksource=tsc radeon.dpm=0 console=tty0 console=ttyS0,115200n8 "
-			"console=uart8250,mmio32,0xd0340000 video=HDMI-A-1:1920x1080-24@60 "
-			"consoleblank=0 net.ifnames=0 drm.debug=0";
 	
+	/* if you want edid loading add this cmd:
+		drm_kms_helper.edid_firmware=edid/my_edid.bin
+	*/
+	
+	char *cmd_line = "panic=0 clocksource=tsc console=tty0 console=ttyS0,115200n8 "
+			"console=uart8250,mmio32,0xd0340000 video=HDMI-A-1:1920x1080-24@60 "
+			"consoleblank=0 net.ifnames=0 drm.debug=0 amdgpu.dpm=0";
+
 	kernel = malloc(kernelsize);
 	initramfs = malloc(initramfssize);
 
